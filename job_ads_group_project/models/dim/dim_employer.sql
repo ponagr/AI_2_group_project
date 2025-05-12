85e5e88fddae665612fca2f56aa82b9e9c6857a9
Pontus Ågren Grundström <pontus.agrengrundstrom@gmail.com>
2025-05-12 10:31:01 +0200

    added some more tests and transformations in the models, to handle null-values, and some more descriptions for documentation

--- job_ads_group_project/models/dim/dim_employer.sql
+++ job_ads_group_project/models/dim/dim_employer.sql
@@ -7,8 +7,8 @@ select
     any_value(employer_organization_number) as employer_organization_number,
     any_value(workplace_country)as workplace_country,
     any_value(workplace_region) as workplace_region,
-    workplace_city,
-    any_value(workplace_street_address) as workplace_street_address,
-    any_value(workplace_postcode) as workplace_postcode
+    coalesce(workplace_city, 'Ej Angiven') as workplace_city,
+    coalesce(any_value(workplace_street_address), 'Ej Angiven') as workplace_street_address,
+    coalesce(any_value(workplace_postcode), 'Ej Angiven') as workplace_postcode
 from dim_employer
 group by employer_workplace, workplace_city
