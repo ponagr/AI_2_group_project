e42bdd0f015d46d692d0b9874d3370d6f6b0c33a
petarfilipovski <pepe.filipovski@gmail.com>
2025-05-14 02:37:14 +0200

    Adding inline documentation comments to src_job_details

--- job_ads_group_project/models/dim/dim_job_details.sql
+++ job_ads_group_project/models/dim/dim_job_details.sql
@@ -1,23 +1,22 @@
 
-with job_details as (
+with dim_job_details as (
     select * from {{ ref('src_job_details') }}
 )
 
 select
-    job_details_id,
-    headline,
-    description,
-    employment_type,
-    duration,
-    salary_type,
-    scope_of_work_min,
-    scope_of_work_max,
-
-    case
-        when scope_of_work_max > 100 then 'Full-time'
-        when scope_of_work_max between 50 and 100 then 'Part-time'
-        else 'Other'
-    end as work_scope_category
-
-from job_details
-where headline is not null
+    {{ dbt_utils.generate_surrogate_key(['job_details_id']) }} as job_details_id,
+    any_value(headline) as headline,
+    any_value(description) as description,
+    any_value(description_html_formatted) as description_html_formatted,
+    any_value(employment_type) as employment_type,
+    any_value(employment_type_concept_id) as employment_type_concept_id,
+    any_value(duration) as duration,
+    any_value(salary_description) as salary_description,
+    any_value(application_deadline) as application_deadline,
+    any_value(publication_date) as publication_date,
+    any_value(vacancies) as vacancies,
+    any_value(conditions) as conditions,
+    any_value(application_url) as application_url,
+    any_value(application_information) as application_information
+from dim_job_details
+group by job_details_id
