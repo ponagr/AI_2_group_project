e50c90141a90da3d0150f4db10a3fe243c966364
petarfilipovski <pepe.filipovski@gmail.com>
2025-05-15 00:54:19 +0200

    Update dim_job_details: remove unnecessary filters, align columns

--- job_ads_group_project/models/dim/dim_job_details.sql
+++ job_ads_group_project/models/dim/dim_job_details.sql
@@ -1,4 +1,3 @@
--- models/dim/dim_job_details.sql
 
 with job_details as (
     select * from {{ ref('src_job_details') }}
@@ -13,12 +12,6 @@ select
     salary_type,
     scope_of_work_min,
     scope_of_work_max,
-
-    case
-        when scope_of_work_max > 100 then 'Full-time'
-        when scope_of_work_max between 50 and 100 then 'Part-time'
-        else 'Other'
-    end as work_scope_category
+    working_hours_type,
 
 from job_details
-where headline is not null
