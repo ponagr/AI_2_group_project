3d5414ab87015958afa24bfa53d129b6ad20f0f4
petarfilipovski <pepe.filipovski@gmail.com>
2025-05-15 00:40:48 +0200

    Update src_job_details: remove unnecessary fields, add salary_type & scope columns

--- job_ads_group_project/models/src/src_job_details.sql
+++ job_ads_group_project/models/src/src_job_details.sql
@@ -1,17 +1,28 @@
--- models/src/src_job_details.sql
 
-with stg_job_details as (
-    select * from {{ source('job_ads', 'stg_job_details') }}
-)
 
+with stg_ads as (
+    select * from {{ source('job_ads', 'stg_ads') }}
+)  -- loading raw job details data from the source table
+
+
+-- Select relevant fields for the src_job_details model
+-- Renaming columns to match dimensional model naming conventions
 select
-    job_details_id,
+    id as job_details_id,  
     headline,
-    description,
-    description_html_formatted,
-    employment_type,
-    duration,
+    description__text as description,
+    description__text_formatted as description_html_formatted,
+    employment_type__label as employment_type,
+    duration__label as duration,
+    salary_description,
+    application_deadline,
+    publication_date,
+    number_of_vacancies as vacancies,
     salary_type,
     scope_of_work_min,
-    scope_of_work_max
-from stg_job_details
+    scope_of_work_max,
+    working_hours_type,
+    
+from stg_ads
+where headline is not null
+
