a0953d244f90083ce0178804a995f25c6d16be75
petarfilipovski <pepe.filipovski@gmail.com>
2025-05-15 00:54:19 +0200

    Update dim_job_details: remove unnecessary filters, align columns

--- job_ads_group_project/models/dim/dim_job_details.sql
+++ job_ads_group_project/models/dim/dim_job_details.sql
@@ -4,19 +4,14 @@ with dim_job_details as (
 )   -- Build the dimension table for job details based on the source model
 
 select
-    {{ dbt_utils.generate_surrogate_key(['job_details_id']) }} as job_details_id,    -- Generating a surrogate key for dimensional modeling  , ensures a unique, consistent primary key for each row in the dimension table
-    any_value(headline) as headline,      
-    any_value(description) as description,
-    any_value(description_html_formatted) as description_html_formatted,
-    any_value(employment_type) as employment_type,
-    any_value(employment_type_concept_id) as employment_type_concept_id,
-    any_value(duration) as duration,
-    any_value(salary_description) as salary_description,
-    any_value(application_deadline) as application_deadline,
-    any_value(publication_date) as publication_date,
-    any_value(vacancies) as vacancies,
-    any_value(conditions) as conditions,
-    any_value(application_url) as application_url,
-    any_value(application_information) as application_information
-from dim_job_details
-group by job_details_id
+    job_details_id,
+    headline,
+    description,
+    employment_type,
+    duration,
+    salary_type,
+    scope_of_work_min,
+    scope_of_work_max,
+    working_hours_type,
+
+from job_details
