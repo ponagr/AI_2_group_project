856c3b4c944e61faecf056885cdc37879c285bc2
petarfilipovski <pepe.filipovski@gmail.com>
2025-05-15 02:38:41 +0200

    Fix src_job_details: apply correct changes to relevant fields

--- job_ads_group_project/models/src/src_job_details.sql
+++ job_ads_group_project/models/src/src_job_details.sql
@@ -13,15 +13,16 @@ select
     description__text as description,
     description__text_formatted as description_html_formatted,
     employment_type__label as employment_type,
-    employment_type__concept_id as employment_type_concept_id,
     duration__label as duration,
     salary_description,
     application_deadline,
     publication_date,
     number_of_vacancies as vacancies,
-    description__conditions as conditions,
-    application_details__url as application_url,
-    application_details__information as application_information
+    salary_type,
+    scope_of_work_min,
+    scope_of_work_max,
+    working_hours_type,
+        
 from stg_ads
 where headline is not null
 
