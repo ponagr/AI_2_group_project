4602ffffe09e588bbd95f5a86382fb96c262bd01
Sandstrom96 <66973334+Sandstrom96@users.noreply.github.com>
2025-05-16 11:45:55 +0200

    Update src_job_details.sql

--- job_ads_group_project/models/src/src_job_details.sql
+++ job_ads_group_project/models/src/src_job_details.sql
@@ -1,5 +1,3 @@
-
-
 with stg_ads as (
     select * from {{ source('job_ads', 'stg_ads') }}
 )  -- loading raw job details data from the source table
@@ -8,21 +6,18 @@ with stg_ads as (
 -- Select relevant fields for the src_job_details model
 -- Renaming columns to match dimensional model naming conventions
 select
-    id as job_details_id,
+    id,
     headline,
     description__text as description,
-    description__text_formatted as description_html_formatted,
+    description__text_formatted as description_html,
     employment_type__label as employment_type,
     duration__label as duration,
     salary_description,
     application_deadline,
     publication_date,
     number_of_vacancies as vacancies,
-    salary_type,
-    scope_of_work_min,
-    scope_of_work_max,
-    working_hours_type,
-        
+    salary_type__label as salary_type,
+    scope_of_work__min as scope_of_work_min,
+    scope_of_work__max as scope_of_work_max,
+    working_hours_type__label as working_hours_type
 from stg_ads
-where headline is not null
-
