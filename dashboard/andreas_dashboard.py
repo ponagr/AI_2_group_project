252329758ac2bc03d6d1fd4c5127a3dc9789e0f5
Sandstrom96 <andreas_sandstrom@protonmail.com>
2025-05-17 00:20:58 +0200

    refactor filter layout to enhance user input options and streamline data filtering

--- dashboard/andreas_dashboard.py
+++ dashboard/andreas_dashboard.py
@@ -46,58 +46,60 @@ def total_vacancies_by_date():
     fig = px.bar(sorted_df, x="occupation_group", y="vacancies", labels={"occupation_group": "Occupation group", "vacancies":"Vacancies"})
     st.plotly_chart(fig)
 
-def total_of_ads():
+def total_of_ads(df):
     st.title("Total number of ads")
-    query = "SELECT COUNT(*) AS total_ads, publication_date, occupation_group FROM mart.mart_installation_maintenence GROUP BY publication_date, occupation_group"
-    df = load_data(query)
     
-    st.subheader(f"Total number of ads for 'occupation_field': {df['total_ads'].sum()}")
+    st.subheader(f"Total number of ads for 'occupation_field': {len(df)}")
     
-    filtered_date_df = get_date(df)
-    grouped_df = filtered_date_df.groupby("occupation_group").size().reset_index(name="total_ads")
+    #filtered_date_df = get_date(df)
+    grouped_df = df.groupby("occupation_group").size().reset_index(name="total_ads")
     sorted_df = grouped_df.sort_values(by="total_ads", ascending=False)
     
     fig = px.bar(sorted_df, x="occupation_group", y="total_ads", labels={"occupation_group": "Occupation group", "total_ads":"Total number of ads"})
     st.plotly_chart(fig)
     
-def layout():
-    st.title("Test")
+def filter_layout(df):
     expander = st.expander("Filter")
-    query = "SELECT * FROM mart.mart_full_job_ads"
-    df = load_data(query)
     
     with expander:
-        col1, col2, col3 = st.columns(3)
-        with col1:
-            occupation = st.selectbox("Filter after occupation", ["All"] + sorted(list(df["occupation_group"].unique())))
-            if occupation != "All":
-                df = df[df["occupation_group"] == occupation]
+        col1, col2 = st.columns(2)
+        occupation_filter = col1.multiselect("Occupation", sorted(df["occupation_group"].unique()), default=[])
+        city_filter = col1.multiselect("City", sorted(df['workplace_city'].unique()), default=[])
+        employer_filter = col1.multiselect("Filter after employer", sorted(df["employer_workplace"].unique()), default=[])
         
         with col2:
-            city = st.selectbox("Filter after city", ["All"] + sorted(list(df["workplace_city"].unique())))
-            if city != "All":
-                df = df[df["workplace_city"] == city]
-
-        with col3:
-            employer = st.selectbox("Filter after employer", ["All"] + sorted(list(df["employer_workplace"].unique())))
-            if employer != "All":
-                df = df[df["employer_workplace"] == employer]
-        st.divider()
-        drivers_license = st.pills("Requires drivers license", ["Yes", "No"], selection_mode="single")
-    if drivers_license == None:
-        st.write("Alla")
+            date_col1, date_col2 = st.columns(2)
+            
+            min_date = df["publication_date"].min().date()
+            max_date = df["publication_date"].max().date()
+            
+            start_date = pd.to_datetime(date_col1.date_input("Start date", min_value=min_date, max_value=max_date, value=min_date))
+            end_date = pd.to_datetime(date_col2.date_input("End date", min_value=start_date, max_value=max_date, value=max_date))
         
+        license_required = st.pills("Requires drivers license", ["Yes", "No"], selection_mode="single")
+        license_required = True if license_required == "Yes" else False if license_required == "No" else None
         
-    pass
-
-def get_date(df):
-    min_date = df["publication_date"].min().date()
-    max_date = df["publication_date"].max().date()
-    
-    start_date = pd.to_datetime(st.date_input("Start date", min_value=min_date, max_value=max_date, value=min_date))
-    end_date = pd.to_datetime(st.date_input("End date", min_value=start_date, max_value=max_date, value=max_date))
-    
-    return df[(df["publication_date"] >= start_date) & (df["publication_date"] <= end_date)]
+        # Apply filters
+        filtered_df = df.copy()
+        
+        if occupation_filter:
+            filtered_df = filtered_df[filtered_df["occupation_group"].isin(occupation_filter)]
+        
+        if city_filter:
+            filtered_df = filtered_df[filtered_df["workplace_city"].isin(city_filter)]
+        
+        if employer_filter:
+            filtered_df = filtered_df[filtered_df["employer_workplace"].isin(employer_filter)]
+        
+        filtered_df = filtered_df[(filtered_df["publication_date"] >= start_date) & (filtered_df["publication_date"] <= end_date)]
+        
+        if license_required is not None:
+            filtered_df = filtered_df[filtered_df["driver_license"] == license_required]
+        
+    return filtered_df
 
 if __name__ == "__main__":
-    layout()
+    query = "SELECT * FROM mart.mart_full_job_ads"
+    df = load_data(query)
+    filtered_df = filter_layout(df)
+    total_of_ads(filtered_df)
