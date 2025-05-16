777fa876a29bbe7a88e80058f0b48f0987e5d463
Sandstrom96 <66973334+Sandstrom96@users.noreply.github.com>
2025-05-16 12:41:25 +0200

    Update dim_job_details.sql

--- job_ads_group_project/models/dim/dim_job_details.sql
+++ job_ads_group_project/models/dim/dim_job_details.sql
@@ -1,17 +1,18 @@
-
-with dim_job_details as (
+with job_details as (
     select * from {{ ref('src_job_details') }}
 )   -- Build the dimension table for job details based on the source model
 
 select
-    job_details_id,
+    {{ dbt_utils.generate_surrogate_key(['id']) }} as job_details_id,
     headline,
     description,
+    description_html,
     employment_type,
-    duration,
+    coalesce(duration, 'ej angiven') as duration,
     salary_type,
+    salary_description,
+    working_hours_type,
     scope_of_work_min,
     scope_of_work_max,
-    working_hours_type,
-
+    publication_date
 from job_details
