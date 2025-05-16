ec32da758d6c3570c22145f7d84f85b1a3fe5eda
Sandstrom96 <andreas_sandstrom@protonmail.com>
2025-05-16 13:43:00 +0200

    Add fct_job_ads.sql model for job advertisements

---
+++ job_ads_group_project/models/fct/fct_job_ads.sql
@@ -0,0 +1,12 @@
+with job_ads as (select * from {{ ref('src_job_ads') }})
+
+select
+    {{ dbt_utils.generate_surrogate_key(['occupation__label']) }} as occupation_id,
+    {{ dbt_utils.generate_surrogate_key(['id']) }} as job_details_id,
+    {{ dbt_utils.generate_surrogate_key(['employer__workplace', 'workplace_address__municipality']) }} as employer_id,
+    {{ dbt_utils.generate_surrogate_key(['_dlt_id']) }} as job_requirements_id,
+    {{ dbt_utils.generate_surrogate_key(['experience_required', 'driving_license_required', 'access_to_own_car']) }} as auxillary_attributes_id,
+    number_of_vacancies as vacancies,
+    relevance,
+    application_deadline
+from job_ads
