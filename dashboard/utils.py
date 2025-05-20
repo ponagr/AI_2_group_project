224e975f41f220d37593f2c84ce1faa462988f3d
Sandstrom96 <andreas_sandstrom@protonmail.com>
2025-05-20 14:55:39 +0200

    Moved utility functions for ordering vacancies and filtering selectboxes

--- dashboard/utils.py
+++ dashboard/utils.py
@@ -15,3 +15,19 @@ def get_df():
     else:
         st.warning("Dataframe not found in session state.")
         st.stop()
+
+# Sortera kolumner efter antal vacancies till selectboxes
+def order_vacancies(df, col):
+    return (df.groupby(col)["Vacancies"].sum().sort_values(ascending=False).index.tolist())
+
+def split_unique_cols(df, column):
+    split_cols = df[column].dropna().str.split(",")
+    strip_cols = [col.strip() for strip_list in split_cols for col in strip_list]
+    return sorted(set(strip_cols))
+
+# Ta bort redan använda kolumner för nästa selectbox
+def filter_selectbox(cols, selected):
+    for string in cols:
+        if string == selected:
+            cols.remove(string)
+    return cols
