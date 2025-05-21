ef9bba2be6c2f892f18cec9d23a74f0a714fb6e2
Andreas Sandström <andreas_sandstrom@protonmail.com>
2025-05-21 13:56:28 +0200

    Refactor barplot_df function to include data cleaning and enhance chart customization for better visualization

--- dashboard/components/plots.py
+++ dashboard/components/plots.py
@@ -1,5 +1,12 @@
 import plotly.express as px
 
 # Enkel barplot med val från selectbox
-def barplot_df(df, column, bar_ammount = 5):
-    return px.bar(df.head(bar_ammount), y="Vacancies", x=column, color=column)
+def barplot_df(df, x_column, y_column="Vacancies", bar_ammount = 5):
+    # drop rows with 'Ej Angiven' in column
+    df = df[(df[x_column] != "Ej Angiven") & (df[x_column] != "Not Specified") & (df[x_column] != "Undefined")]
+    fig = px.bar(df.head(bar_ammount), y=y_column, x=x_column, color=x_column)
+    fig.update_layout(showlegend=False)
+    if x_column == "Driver License" or x_column == "Experience Required":
+        fig.update_layout(yaxis_title="Krav på körkort")
+        fig.update_xaxes(tickvals=[True, False], ticktext=["Ja", "Nej"])
+    return fig
