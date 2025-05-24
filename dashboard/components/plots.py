d3e9e7e633bb5d7c26024a649720e690bdda3b5f
Sandstrom96 <andreas_sandstrom@protonmail.com>
2025-05-24 14:19:26 +0200

    add line and pie charts plotting

--- dashboard/components/plots.py
+++ dashboard/components/plots.py
@@ -1,4 +1,5 @@
 import plotly.express as px
+import streamlit as st
 
 # Enkel barplot med val från selectbox
 def barplot_df(df, x_column, y_column="Vacancies", bar_ammount = 5):
@@ -9,4 +10,23 @@ def barplot_df(df, x_column, y_column="Vacancies", bar_ammount = 5):
     if x_column == "Driver License" or x_column == "Experience Required":
         fig.update_layout(yaxis_title="Krav på körkort")
         fig.update_xaxes(tickvals=[True, False], ticktext=["Ja", "Nej"])
-    return fig
+    st.plotly_chart(fig)
+
+def lineplot_df(df, x_column="Publication Date", y_column="Vacancies", bar_ammount = 5, color_column=None):
+    if color_column:
+        # drop rows with 'Ej Angiven' in column
+        df = df[(df[x_column] != "Ej Angiven") & (df[x_column] != "Not Specified") & (df[x_column] != "Undefined")]
+        
+        top_categories = df[color_column].value_counts().nlargest(bar_ammount).index
+        df = df[df[color_column].isin(top_categories)]
+
+    fig = px.line(df, x=x_column, y=y_column, color=color_column)
+    fig.update_layout(showlegend=True)
+    st.plotly_chart(fig)
+
+def pieplot_df(df, x_column, y_column="Vacancies", bar_ammount = 5):
+    # drop rows with 'Ej Angiven' in column
+    df = df[(df[x_column] != "Ej Angiven") & (df[x_column] != "Not Specified") & (df[x_column] != "Undefined")]
+    fig = px.pie(df.head(bar_ammount), values=y_column, names=x_column)
+    fig.update_traces(textposition='inside', textinfo='percent')
+    st.plotly_chart(fig)
