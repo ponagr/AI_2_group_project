f4c60ad19205454aab5efeed38a3fd2dd9d209d3
Sandstrom96 <andreas_sandstrom@protonmail.com>
2025-05-24 14:20:15 +0200

    refactor: update dashboard views to enhance plotting functionality and streamline data display

--- dashboard/components/dashboard_views.py
+++ dashboard/components/dashboard_views.py
@@ -1,7 +1,7 @@
 import streamlit as st
-import pandas as pd
-from utils.utils import filter_df, aggregate_by_group
+from utils.utils import aggregate_by_group
 from components.kpis import show_metrics
+from components.plots import barplot_df, lineplot_df, pieplot_df
 
 import plotly.express as px
 
@@ -12,29 +12,52 @@ def metrics_view(df, column=None):
         st.markdown(f"## Top 5: {column}s")
         show_metrics(df, column, "Vacancies", 5)
 
-def plot_tab(df, column=None):
-    
-    bar, line, pie = st.tabs(["Barchart", "Linechart", "Piechart"])
-    # line_chart_df = df.groupby(["Publication Date", "Occupation Field"]).size().reset_index(name="Total Ads")
+def show_newest_ads(df):
+        df = df.sort_values("Publication Date", ascending=False).head(20).reset_index()
+        expander = st.expander("20 latest job ads", expanded=True)
+        expander.dataframe(df[["Occupation", "Employer Name", "Workplace City", "Publication Date"]])
 
-    line_chart_df = df.groupby(["Publication Date", column]).size().reset_index(name="Total Ads")
-    df = aggregate_by_group(df, column)
-    num_groups = st.slider("Number of groups to show", min_value=1, max_value=10, value=5)
-    with bar:
-        st.markdown("### Total vacancies for top " + str(num_groups) + " " + column)
-        fig = px.bar(df.head(num_groups), x=column, y="Vacancies")
-        st.plotly_chart(fig)
+def plot_tab(df, column=None):
+    columns = ["Occupation Field", "Occupation Group", "Occupation",
+                "Workplace City", "Employer Workplace", "Salary Description",
+                "Duration", "Working Hours Type", "Driver License", "Experience Required"]
     
-    with line:
-        st.markdown("### Total job ads for top " + str(num_groups) + " " + column + " by publication date")
-        fig = px.line(line_chart_df.head(num_groups), x="Publication Date", y="Total Ads", color=column)
-        st.plotly_chart(fig)
+    if column is None:
+        selection = st.pills("select column:", columns, default="Occupation Field", label_visibility="hidden")
+        
+        if selection == "Occupation Field":
+            vacancies_over_time_df = df.groupby("Publication Date").size().reset_index(name="Vacancies")
+            color_col = None
+        else:
+            vacancies_over_time_df = df.groupby(["Publication Date", selection]).size().reset_index(name="Vacancies")
+            color_col = selection
+        
+        df = aggregate_by_group(df, selection)
+        
+        bar, line, pie = st.tabs(["Barchart", "Linechart", "Piechart"])
+        with bar:
+            barplot_df(df, selection)
+        
+        with line:
+            lineplot_df(vacancies_over_time_df, x_column="Publication Date", y_column="Vacancies", color_column=color_col)
+        
+        with pie:  
+            pieplot_df(df, selection)
     
-    with pie:
-        st.markdown("### Total vacancies for top " + str(num_groups) + " " + column)
+    else:
+        line_chart_df = df.groupby(["Publication Date", column]).size().reset_index(name="Total Ads")
         df = aggregate_by_group(df, column)
-        fig = px.pie(df.head(num_groups), values="Vacancies", names=column)
-        st.plotly_chart(fig)
+        
+        bar, line, pie = st.tabs(["Barchart", "Linechart", "Piechart"])
+        num_groups = st.slider("Number of groups to show", min_value=1, max_value=10, value=5)
+        with bar:
+            barplot_df(df, column, y_column="Vacancies", bar_ammount=num_groups)
+        
+        with line:
+            lineplot_df(line_chart_df, x_column="Publication Date", y_column="Total Ads", color_column=column)
+        
+        with pie:
+            pieplot_df(df, column, y_column="Vacancies", bar_ammount=num_groups)
 
 def desc_tab(df):
     st.markdown("### Jobbannonsbeskrivning")
