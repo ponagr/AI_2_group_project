b325e290131baca92883a2ad8f9d01e49f3c5937
Pontus Ågren Grundström <pontus.agrengrundstrom@gmail.com>
2025-05-26 17:22:06 +0200

    updated requirements.txt, cleaned up some unused imports in page files

--- dashboard/pages/2_analytics_by_city.py
+++ dashboard/pages/2_analytics_by_city.py
@@ -1,6 +1,4 @@
 import streamlit as st
-import pandas as pd
-import plotly.express as px
 from components.sidebar import render_sidebar
 from components.filter_tab import filter_tab
 from components.dashboard_views import plot_tab, metrics_view
