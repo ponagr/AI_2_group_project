3b4faf7e354a50953bdaf6e5b3d4d2c014c8682c
Sandstrom96 <andreas_sandstrom@protonmail.com>
2025-05-26 15:27:20 +0200

    feat: add new models and enhance existing ones with tests and documentations

--- job_ads_group_project/models/src/src_job_ads.sql
+++ job_ads_group_project/models/src/src_job_ads.sql
@@ -1,3 +1,16 @@
 with job_ads as (select * from {{ source('job_ads', 'stg_ads') }})
 
-select * from job_ads
+select 
+    id,
+    _dlt_id,
+    occupation__label,
+    employer__workplace,
+    workplace_address__municipality,
+    experience_required,
+    driving_license_required,
+    access_to_own_car,
+    number_of_vacancies,
+    relevance,
+    publication_date,
+    application_deadline
+from job_ads
